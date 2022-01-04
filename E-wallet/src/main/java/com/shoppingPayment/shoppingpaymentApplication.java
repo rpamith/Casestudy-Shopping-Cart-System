bddebe739d58bd4ec044cfed2d4c169168cc519e
ramithku <ramithku@LIN33005531.corp.capgemini.com>
2022-01-04 12:19:39 +0530

    COMMIT AND PUSH

---
+++ E-wallet/src/main/java/com/shoppingPayment/shoppingpaymentApplication.java
@@ -0,0 +1,17 @@
+package com.shoppingPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.shoppingPayment.shoppingpaymentApplication;
+
+@SpringBootApplication
+
+
+public class shoppingpaymentApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(shoppingpaymentApplication.class, args);
+	}
+
+}
